9d1e42d5ed8ed9f15f1e581fd21ca340e3cb348f
姚超超 <1392241352@qq.com>
2025-07-15 17:07:47 +0800

    feat: 对接锡商行接口

--- api/configs/wxsbank/bank/bank_config.py
+++ api/configs/wxsbank/bank/bank_config.py
@@ -5,5 +5,5 @@ from pydantic_settings import BaseSettings
 class BankConfig(BaseSettings):
     WXSBANK_URL: str = Field(
         description="BankConfig",
-        default=False,
+        default="https://47.109.146.94:8059/htjx-api/",
     )
